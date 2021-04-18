6ea068bb3a47d09cdebab38fe0896dcb6abb61e4
haojn <haojn@haojndeMacBook-Pro.local>
2021-04-18 16:56:04 +0800

    update file

--- cloud-consumer-order80/src/main/java/com/ai/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/ai/cloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ai.cloud.controller;
 
 import com.ai.cloud.entities.CommonResult;
 import com.ai.cloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class OrderController {
     public static final String PAYMENT_URL = "http://localhost:8001";
     @Resource
